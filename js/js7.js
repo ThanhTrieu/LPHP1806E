eaf7ec32602363a70966ca138cbbc2ae29249c3c
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2018-09-05 23:49:47 +0700

    update code

---
+++ js/js7.js
@@ -0,0 +1,6 @@
+// thao tac voi BOM trong js - thao tac voi trinh duyet
+// xac dinh kich thuoc cua trinh duyet
+// BOM - Window
+let w = window.innerWidth;
+let h = window.innerHeight;
+console.log(`chieu rong la: ${w} - chieu cao la: ${h}`);
